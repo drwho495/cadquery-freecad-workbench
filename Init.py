55250383b1d65c7e523781807842bf70ccf3678b
Jeremy Wright <wrightjmf@gmail.com>
2025-01-02 16:25:15 -0500

    Have to install dependencies manually because of conda environment

--- Init.py
+++ Init.py
@@ -7,10 +7,18 @@ globals()['show_object'] = show_object
 try:
     import cadquery
 except ImportError:
+    print("CadQuery is not installed. Installing now...")
+
     import os
     import sys
     import subprocess
-    os.environ.pop('CONDA_PREFIX', None)
-    os.environ.pop('CONDA_PREFIX_1', None)
-    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "--force-reinstall", "cadquery==2.5.1"], capture_output=True)
-    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "--force-reinstall", "cadquery_ocp==7.7.2"], capture_output=True)
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "cadquery==2.5.1"], capture_output=True)
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "cadquery-ocp==7.7.2"], capture_output=True)
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "ezdxf"], capture_output=True)
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "multimethod>=1.11,<2.0"], capture_output=True)
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "nlopt>=2.9.0,<3.0"], capture_output=True)
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "typish"], capture_output=True)
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "casadi"], capture_output=True)
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "path"], capture_output=True)
+
+    print("CadQuery has been installed. Please restart FreeCAD.")
