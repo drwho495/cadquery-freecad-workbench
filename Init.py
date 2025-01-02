5e6bb0e6a175c3f1a0b50539d900a8ef35d3b448
Jeremy Wright <wrightjmf@gmail.com>
2025-01-02 16:06:14 -0500

    Trying to make sure dependencies are installed on workbench start

--- Init.py
+++ Init.py
@@ -2,3 +2,12 @@ from CQGui.display import show_object
 
 # Register the show_object function as a global function
 globals()['show_object'] = show_object
+
+# Make sure the CadQuery packages are installed
+try:
+    import cadquery
+except ImportError:
+    import sys
+    import subprocess
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "cadquery==2.5.1"], capture_output=False)
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "cadquery_ocp==7.7.2"], capture_output=False)
