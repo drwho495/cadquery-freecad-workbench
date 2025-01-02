b14448d131288e5f428a25e57a4ea1e87d3d27af
Jeremy Wright <wrightjmf@gmail.com>
2025-01-02 16:18:36 -0500

    Trying to make sure the user knows what is going on

--- Init.py
+++ Init.py
@@ -12,5 +12,5 @@ except ImportError:
     import subprocess
     os.environ.pop('CONDA_PREFIX', None)
     os.environ.pop('CONDA_PREFIX_1', None)
-    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "--force-reinstall", "cadquery==2.5.1"], capture_output=False)
-    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "cadquery_ocp==7.7.2"], capture_output=False)
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "--force-reinstall", "cadquery==2.5.1"], capture_output=True)
+    subprocess.run(["python", "-m", "pip", "install", "--upgrade", "--force-reinstall", "cadquery_ocp==7.7.2"], capture_output=True)
