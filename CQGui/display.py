5c11f3f7a95ca154be66ff002e8ca7c20e60dd58
Jeremy Wright <wrightjmf@gmail.com>
2025-01-07 15:27:42 -0500

    Handle Build123d objects in show_object

--- CQGui/display.py
+++ CQGui/display.py
@@ -1,11 +1,43 @@
 # Define the show_object function which CadQuery execution environments need to provide
 def show_object(cq_object, options=None):
+    import cadquery as cq
     import Part, FreeCAD, FreeCADGui
     from PySide import QtGui
 
     # Create the object that the BRep data will be read into
     from io import BytesIO
     brep_stream = BytesIO()
+
+    # Keep track of the feature name
+    feature_name = None
+    # Check to see what type of object we are dealing with
+    if isinstance(cq_object, cq.Workplane):
+        # Handle the label if the user set it
+        if cq_object.val().label:
+            feature_name = cq_object.val().label
+
+        # If we have a workplane, we need to convert it to a solid
+        cq_object.val().exportBrep(brep_stream)
+    elif isinstance(cq_object, cq.Shape):
+        # If we have a solid, we can export it directly
+        cq_object.exportBrep(brep_stream)
+    elif hasattr(cq_object, "wrapped"):
+        # Handle the label if the user has it set
+        if hasattr(cq_object, "label"):
+            feature_name = cq_object.label
+
+        from build123d import export_brep
+        export_brep(cq_object, brep_stream)
+    elif hasattr(cq_object, "_obj"):
+        # Handle the label if the user has it set
+        if hasattr(cq_object, "label"):
+            feature_name = cq_object.label
+
+        from build123d import export_brep
+        export_brep(cq_object._obj, brep_stream)
+    else:
+        print("Object type not suuport for display: ", type(cq_object).__name__)
+        return
 
     # Get the title of the current document so that we can create/find the FreeCAD part window
     doc_name = "untitled"
@@ -25,7 +57,6 @@ def show_object(cq_object, options=None):
     ad = FreeCAD.activeDocument()
 
     # Convert the CadQuery object to a BRep string and then into a FreeCAD part shape
-    rv = cq_object.val().exportBrep(brep_stream)
     brep_string = brep_stream.getvalue().decode('utf-8')
     part_shape = Part.Shape()
     part_shape.importBrepFromString(brep_string)
@@ -33,9 +64,8 @@ def show_object(cq_object, options=None):
     # options={"alpha":0.5, "color": (64, 164, 223)}
 
     # If the user wanted to use a specific name in the tree, use it
-    feature_name = doc_name
-    if cq_object.val().label:
-        feature_name = cq_object.val().label
+    if not feature_name:
+        feature_name = doc_name
 
     # Find the feature in the document, if it exists
     cur_feature = None
